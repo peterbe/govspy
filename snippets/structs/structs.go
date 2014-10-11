e504b7db902f90c15c719ec63b9b1c39b67e9cd4
Peter Bengtsson <mail@peterbe.com>
2014-10-11 14:27:36 -0700

    moved

--- snippets/structs/structs.go
+++ snippets/structs/structs.go
@@ -14,8 +14,13 @@ func distance(point1 Point, point2 Point) float64 {
 	return math.Sqrt(point1.x * point2.x + point1.y * point2.y)
 }
 
+func distance_better(point1 *Point, point2 *Point) float64 {
+	return math.Sqrt(point1.x * point2.x + point1.y * point2.y)
+}
+
 func main() {
 	p1 := Point{1, 3}
 	p2 := Point{2, 4}
 	fmt.Println(distance(p1, p2)) // 3.7416573867739413
+	fmt.Println(distance_better(&p1, &p2)) // 3.7416573867739413
 }
