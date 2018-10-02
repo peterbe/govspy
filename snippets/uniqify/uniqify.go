072d78aacd029e6c809d61fd74dad284b29c92bb
lootek <piotr.lucinski@solarwinds.cloud>
2018-10-02 09:32:53 +0200

    Rewritten uniqify() in Go

--- snippets/uniqify/uniqify.go
+++ snippets/uniqify/uniqify.go
@@ -2,21 +2,26 @@ package main
 
 import "fmt"
 
-func uniqify(items *[]string) {
+func uniqify(items []string) []string {
+	uniq := make([]string, 0)
 	seen := make(map[string]bool)
-	j := 0
-	for i, x := range *items {
-		if !seen[x] {
-			seen[x] = true
-			(*items)[j] = (*items)[i]
-			j++
+
+	// For the highest memory efficiency, do:
+	// seen := make(map[string]struct{})
+	// see: https://stackoverflow.com/questions/37320287/maptstruct-and-maptbool-in-golang
+
+	for _, i := range items {
+		if _, exists := seen[i]; !exists {
+			uniq = append(uniq, i)
+			seen[i] = true
 		}
 	}
-	*items = (*items)[:j]
+
+	return uniq
 }
 
 func main() {
 	items := []string{"B", "B", "E", "Q", "Q", "Q"}
-	uniqify(&items)
+	items = uniqify(items)
 	fmt.Println(items) // prints [B E Q]
 }
